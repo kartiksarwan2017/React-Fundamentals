3e2d13340c1d799a8681791839f9d52bcc0bbb22
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-08-04 23:58:22 +0530

    Hello World

---
+++ src/LikeButton.js
@@ -0,0 +1,12 @@
+class LikeButton extends React.Component {
+    render() {
+        return React.createElement('button', null, 'Like');
+    }
+}
+
+
+const app = document.getElementById('app');
+ReactDOM.render(
+    React.createElement(LikeButton),
+    app
+);
