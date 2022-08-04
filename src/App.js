1a9517f8c9c52e85332bf2e80f20a5a39ddaccc8
unknown <kartiksarwan2017.comp@mmcoe.edu.in>
2022-08-05 00:45:43 +0530

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   const name = 'Rahul';
+  const loading = true;
+
+  if(loading) {
+    return <h1>loading!</h1>
+  }
   return (
+    // <React.Fragment>
+    // <h1>Hey there!</h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -21,6 +29,7 @@ function App() {
         </a>
       </header>
     </div>
+    // </React.Fragment>
   );
 }
 
